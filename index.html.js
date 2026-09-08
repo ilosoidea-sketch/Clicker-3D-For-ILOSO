9debd8869e6f32ad85f952a332d7174eb7425163
ilosoidea-sketch <iloso.idea@gmail.com>
2026-09-08 23:29:03 +0700

    Delete index.html.js

--- index.html.js
+++
@@ -1,7 +0,0 @@
-import { initCamera } from './camera.js';
-import { initObjectStudio } from './object.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-  initCamera();
-  initObjectStudio();
-});
